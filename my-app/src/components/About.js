d920e1e01d08aa479d5b441b779a9b7e2e5043e7
Josh Papermaster <jpapermaster@utexas.edu>
2019-10-22 20:14:20 -0500

    Added github api

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -8,6 +8,45 @@ import Zachary from '../assets/images/Zachary_Profile_Pic.jpg'
 import './About.css'
 
 class About extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        rohangit: {
+          total: null
+        },
+        punitgit: {
+          total: null
+        },
+        zachgit: {
+          total: null
+        },
+        thiensongit: {
+          total: null
+        },
+        joshgit: {
+          total: null
+        },
+        armangit: {
+          total: null
+        }
+      };
+    }
+
+    // Apologize for the ugly code but this was the only way to determine who is who since the order changes
+    componentDidMount() {
+      fetch('https://api.github.com/repos/armankhondker/ee-461l-linedrivebetting/stats/contributors?access_token=4498e22ed10af7668695cb5afa35c77ccf635b62&anon=true')
+        .then(response => response.json())
+        .then(data => this.setState({
+          rohangit: data[0].author.login == "rohanvgarg" ? data[0] : data[1].author.login == "rohanvgarg" ? data[1] : data[2].author.login == "rohanvgarg" ? data[2] : data[3].author.login == "rohanvgarg" ? data[3] : data[4].author.login == "rohanvgarg" ? data[4] : data[5],
+          punitgit: data[0].author.login == "PatelPunit" ? data[0] : data[1].author.login == "PatelPunit" ? data[1] : data[2].author.login == "PatelPunit" ? data[2] : data[3].author.login == "PatelPunit" ? data[3] : data[4].author.login == "PatelPunit" ? data[4] : data[5],
+          zachgit: data[0].author.login == "zherink" ? data[0] : data[1].author.login == "zherink" ? data[1] : data[2].author.login == "zherink" ? data[2] : data[3].author.login == "zherink" ? data[3] : data[4].author.login == "zherink" ? data[4] : data[5],
+          thiensongit: data[0].author.login == "thienson-ho" ? data[0] : data[1].author.login == "thienson-ho" ? data[1] : data[2].author.login == "thienson-ho" ? data[2] : data[3].author.login == "thienson-ho" ? data[3] : data[4].author.login == "thienson-ho" ? data[4] : data[5],
+          joshgit: data[0].author.login == "joshpapermaster" ? data[0] : data[1].author.login == "joshpapermaster" ? data[1] : data[2].author.login == "joshpapermaster" ? data[2] : data[3].author.login == "joshpapermaster" ? data[3] : data[4].author.login == "joshpapermaster" ? data[4] : data[5],
+          armangit: data[0].author.login == "ArmanKhondker" ? data[0] : data[1].author.login == "ArmanKhondker" ? data[1] : data[2].author.login == "ArmanKhondker" ? data[2] : data[3].author.login == "ArmanKhondker" ? data[3] : data[4].author.login == "ArmanKhondker" ? data[4] : data[5],
+        }));
+    }
+
     render()
     {
         return(
@@ -41,7 +80,7 @@ class About extends React.Component {
                <br></br>
                Responsibilites: Front End Development in React, Set up website hosting, Set up about page, Wrote User Stories, Design Report
                <br></br>
-               Number of Commits:
+               Number of Commits: {this.state.armangit.total}
                <br></br>
                Total Number of Unit Tests:
                <br></br>
@@ -56,9 +95,9 @@ class About extends React.Component {
                <br></br>
                Major: Electrical and Computer Engineering
                <br></br>
-               Responsibilites: Backend development using AWS S3, Set up MongoDB, Set up website hosting, Web scraping
+               Responsibilites: Backend development using AWS S3, Set up MongoDB, Built API using Node.js, Set up website hosting, Web scraping
                <br></br>
-               Number of Commits:
+               Number of Commits: {this.state.joshgit.total}
                <br></br>
                Total Number of Unit Tests:
                <br></br>
@@ -72,11 +111,11 @@ class About extends React.Component {
               <br></br>
                Bio:A senior Computer Engineering student with technical cores in Software Engineering and Academic Enrichmnent
                <br></br>
-               Major: Electrical and Computer Engineering 
+               Major: Electrical and Computer Engineering
                <br></br>
                Responsibilites: Front End Development, User Interface, and Design Report
                <br></br>
-               Number of Commits:
+               Number of Commits: {this.state.punitgit.total}
                <br></br>
                Total Number of Unit Tests:
                <br></br>
@@ -93,7 +132,7 @@ class About extends React.Component {
                <br></br>
                Responsibilites: Back End Development, Data Collection, Web Scraping, API Handling
                <br></br>
-               Number of Commits:
+               Number of Commits: {this.state.zachgit.total}
                <br></br>
                Total Number of Unit Tests:
                <br></br>
@@ -110,7 +149,7 @@ class About extends React.Component {
                <br></br>
                Responsibilites: Front End Development, User Interface
                <br></br>
-               Number of Commits:
+               Number of Commits: {this.state.thiensongit.total}
                <br></br>
                Total Number of Unit Tests:
                <br></br>
@@ -119,15 +158,15 @@ class About extends React.Component {
            </ul>
 
            <ul>Rohan Garg
-           { <img class="prof_pic" src ={Rohan} alt = "Rohan" /> }    
+           { <img class="prof_pic" src ={Rohan} alt = "Rohan" /> }
 	       <br></br>
-               Bio: A senior Computer Engineering student with technical cores in Software Engineering and Academic Enrichment. 
+               Bio: A senior Computer Engineering student with technical cores in Software Engineering and Academic Enrichment.
                <br></br>
                Major: Electrical and Computer Engineering.
                <br></br>
                Responsibilites: Backend Development, User Interface, and  Design Report
                <br></br>
-               Number of Commits:
+               Number of Commits: {this.state.rohangit.total}
                <br></br>
                Total Number of Unit Tests:
                <br></br>
@@ -136,15 +175,15 @@ class About extends React.Component {
            </ul>
 
            <p><h1>Data</h1>
-           We will scrape data from the following websites: 
+           We will scrape data from the following websites:
            ESPN.com, FiveThirtyEight.com ,The-odds-api.com, Oddshark.com
            <br></br>
-           The data we will specifically be pulling are: the scores of previous games, the former head to head results, and all betting lines (money line, point spread, and over/under) for all NBA, NFL, and MLB games. The data will be scraped from the following websites and API endpoints: 
+           The data we will specifically be pulling are: the scores of previous games, the former head to head results, and all betting lines (money line, point spread, and over/under) for all NBA, NFL, and MLB games. The data will be scraped from the following websites and API endpoints:
            <br></br>
             http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard (NFL scores)
-            <br></br> 
+            <br></br>
             http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard (NBA scores)
-            <br></br> 
+            <br></br>
             http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard (MLB scores)
             <br></br>
             https://data.fivethirtyeight.com/ (NFL, NBA, and MLB predictions)
@@ -158,21 +197,21 @@ class About extends React.Component {
             https://www.oddsshark.com/mlb/odds (MLB odds)
             <br></br>
 
-          
+
            </p>
 
            <p><h1>Tools</h1>
             React: Front End Javascript Framework
             <br></br>
             Github API: User stats dynamically pulled from Github.com
-            <br></br> 
+            <br></br>
             Amazon Web Services: Hosting of web applicaiton
             <br></br>
-            Python: Backend webscrapping 
+            Python: Backend webscrapping
             <br></br>
             MongoDB: Database to store our scrapping data
             <br></br>
-            Namecheap: Purchase linedrivebetting domain 
+            Namecheap: Purchase linedrivebetting domain
             <br></br>
            </p>
 
