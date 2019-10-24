191e60172c2fc89c4844d6526ef56d1b721030c3
Josh Papermaster <jpapermaster@utexas.edu>
2019-10-24 14:53:57 -0500

    Created and deployed accounts api and changed readme for other api

---
+++ login-api/app.js
@@ -0,0 +1,39 @@
+'use strict'
+// Import express
+let express = require('express');
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import Mongoose
+let mongoose = require('mongoose');
+// Initialise the app
+let app = express();
+
+// Import routes
+let apiRoutes = require("./api-routes");
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+// Connect to Mongoose and set connection variable
+// mongoose.connect('mongodb://localhost/resthub', { useNewUrlParser: true});
+mongoose.connect('mongodb+srv://linedrivebetting:texaslonghorns@cluster0-w7pi2.mongodb.net/user_data', { useNewUrlParser: true});
+
+var db = mongoose.connection;
+// Added check for DB connection
+if(!db)
+    console.log("Error connecting db")
+else
+    console.log("Db connected successfully")
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Use Api routes in the App
+app.use('/api', apiRoutes);
+
+//Launch to lambda
+module.exports = app
