e216984b7f48f635a640e8dd51ecdf33382120f6
Josh Papermaster <jpapermaster@utexas.edu>
2019-10-24 18:50:55 -0500

    Login functionality progress

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -9,6 +9,91 @@ import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import Button from "react-bootstrap/Button"
 
 class Login extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state= {
+        data: [
+          {
+            username: null,
+            password: null
+          }
+        ],
+        username: "",
+        password: ""
+        };
+        this.checkIfAccountExists = this.checkIfAccountExists.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.createAccount = this.createAccount.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    checkIfAccountExists() {
+      fetch('https://e6x9m59wb1.execute-api.us-east-1.amazonaws.com/latest/api/accounts')
+        .then(response => response.json())
+        .then(data => this.setState({
+          data: data
+        }), (error) => {
+          if (error) {
+            console.log(error)
+          }
+      });
+      var i;
+      for (i=0; i<this.state.data.length; i++) {
+        if(this.state.username === this.state.data[i].username)
+          return true;
+      }
+      return false;
+    }
+
+    handleLogin(event) {
+      event.preventDefault()
+      console.log(event)
+      // fetch('https://e6x9m59wb1.execute-api.us-east-1.amazonaws.com/latest/api/accounts')
+      //   .then(response => response.json())
+      //   .then(data => this.setState({
+      //     data: data
+      //   }), (error) => {
+      //     if (error) {
+      //       console.log(error)
+      //     }
+      // });
+      // var i;
+      // for (i=0; i<this.state.data.length; i++) {
+      //   if(this.state.username === this.state.data[i].username && this.state.password == this.state.data[i].password)
+      //     return true;
+      // }
+      // return false;
+    }
+
+    createAccount(event) {
+      event.preventDefault()
+      console.log(this.state.username)
+      console.log(this.state.password)
+      // if (this.checkIfAccountExists() == true) {
+      //   return;
+      // }
+      // fetch('https://mywebsite.com/endpoint/', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({
+      //     username: this.state.username,
+      //     password: this.state.password,
+      //   })
+      }
+
+
+    handleUsernameChange(e) {
+       this.setState({username: e.target.value});
+    }
+    handlePasswordChange(e) {
+       this.setState({password: e.target.value});
+    }
+
     render()
     {
         return(
@@ -17,33 +102,33 @@ class Login extends React.Component {
                  <h1>Placeholder</h1>
                  <img src={Logo} className="App-logo-pages" alt="logo" />
 
-                 <Form>
+             <Form>
 
-            <Form.Group as={Row} controlId="formPlaintextPassword">
+            <Form.Group as={Row} className = "login" controlId="formPlaintextPassword">
                 <Form.Label column sm="3">
                 Username
                 </Form.Label>
                 <Col sm="6">
-                <Form.Control type="username" placeholder="Username" />
+                <Form.Control type="username" placeholder="Username" value={this.state.username} onChange={this.handleUsernameChange} />
                 </Col>
             </Form.Group>
 
-            <Form.Group as={Row} controlId="formPlaintextPassword">
+            <Form.Group as={Row} className = "login" controlId="formPlaintextPassword">
                 <Form.Label column sm="3">
                 Password
                 </Form.Label>
                 <Col sm="6">
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange} />
                 </Col>
             </Form.Group>
             </Form>
 
 
             <ButtonToolbar>
-                <Button className="LoginButton" variant="danger">Login</Button>
-                <Button className="CreateAccount" variant="light">Create Account</Button>
+                <Button onClick={this.handleLogin} className="LoginButton" variant="danger">Login</Button>
+                <Button onClick={this.createAccount} className="CreateAccount" variant="light">Create Account</Button>
             </ButtonToolbar>
-               </body> 
+               </body>
                </div>
         );
     }
