43381be8d7cc67d9d71446a47c917cd94371fd4f
Josh Papermaster <jpapermaster@utexas.edu>
2019-11-14 11:51:35 -0600

    Added first verion of a like button

--- my-app/src/components/BlogPost.js
+++ my-app/src/components/BlogPost.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 class BlogPost extends React.Component {
 
@@ -8,6 +9,7 @@ class BlogPost extends React.Component {
         <p className="post-username">username:</p>
         <p className="post-content">content goes here</p>
         <p className="like-count">8 likes</p>
+        <Button className="like-button" variant="primary">Like</Button>
       </div>
     )
   }
