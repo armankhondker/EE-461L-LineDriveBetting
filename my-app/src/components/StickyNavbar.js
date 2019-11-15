fec9a650329a589b15601df82d9b0afe09b5abcd
Arman <armankhondker@utexas.edu>
2019-11-15 13:01:17 -0600

    Phase 3: Update bars at top to not be redundant

--- my-app/src/components/StickyNavbar.js
+++ my-app/src/components/StickyNavbar.js
@@ -14,7 +14,7 @@ function StickNavbar() {
         <Navbar bg="dark" variant="dark" fixed="top">
             <Navbar.Brand href="/">LineDriveBetting</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
+               {/* <Nav.Link href="/">Home</Nav.Link> */}
                 <Nav.Link href="/About">About</Nav.Link>
                 <Nav.Link href="/Blog">Blog</Nav.Link>
                 {
