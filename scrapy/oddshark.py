50467b1d815154a43efa08c60e45686cb606e5ca
Zachary <zachary.herink@utexas.edu>
2019-12-02 11:15:54 -0600

    removed useless code

--- scrapy/oddshark.py
+++ scrapy/oddshark.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 
 import time
 import sys
